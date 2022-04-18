9d816f54523498edabeadf5b2b01d590859dbbe2
Yulia <mme.yuliaiakovleva@yandex.ru>
2022-04-18 01:28:31 +0000

    перенести проект на удаленный сервер

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -15,18 +15,14 @@ class Api {
 
     getInitialCards() {
         return fetch(`${this._url}/cards`, {
-        headers:  {
-            'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
-           }
+            headers: this._headers,
         })
         .then(this._checkResponse)
     };
 
     getUserInfo() {
         return fetch(`${this._url}/users/me`, {
-            headers:  {
-                'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
-            }
+            headers: this._headers,
         })
         .then(this._checkResponse)
  
@@ -35,10 +31,7 @@ class Api {
     setUserInfo(inputValue) {
         return fetch(`${this._url}/users/me`, {
             method: 'PATCH',
-            headers: {
-               'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
-                ...this.headers
-            },
+            headers: this._headers,
             body: JSON.stringify({
                 name: inputValue.name,
                 about: inputValue.about,
@@ -52,10 +45,7 @@ class Api {
     addNewCard(data) {
         return fetch(`${this._url}/cards`, {
             method: 'POST',
-            headers: {
-               'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
-                ...this.headers
-            },
+            headers: this._headers,
             body: JSON.stringify({
                 name: data.name,
                 link: data.link
@@ -68,10 +58,7 @@ class Api {
     changeLikeCardStatus(cardId, isLiked) {
         return fetch(`${this._url}/cards/likes/${cardId}`, {
             method: `${isLiked ? 'DELETE' : 'PUT'}`,
-            headers: {
-               'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
-                ...this.headers
-            },
+            headers: this._headers,
           })
         .then(this._checkResponse) 
     }
@@ -79,10 +66,7 @@ class Api {
     deleteCard(cardId){
         return fetch(`${this._url}/cards/${cardId}`, {
             method: 'DELETE',
-            headers: {
-               'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
-                ...this.headers
-            },
+            headers: this._headers,
           })
         .then(this._checkResponse)
  
@@ -91,10 +75,7 @@ class Api {
     changeAvatar(link){
         return fetch(`${this._url}/users/me/avatar`, {
             method: 'PATCH',
-            headers: {
-               'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
-                ...this.headers
-            },
+            headers: this._headers,
             body: JSON.stringify({avatar: link})
           })
         .then(this._checkResponse)
@@ -106,7 +87,7 @@ class Api {
 const api = new Api({
     url: 'http://api.mesto.yuliayakovleva.nomoredomains.work',
     headers: {
-    //   authorization: `Bearer ${localStorage.jwt}`,
+      authorization: `Bearer ${localStorage.jwt}`,
       'Content-Type': 'application/json'
     }
   });
